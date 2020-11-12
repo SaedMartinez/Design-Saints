44afef787506a31c2b95bd7f03ca7e4d792c4dfe
saed martinez <smartinez4@misena.edu.co>
2020-11-11 23:57:31 -0500

    Done home page wout href

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 window.addEventListener('load', () => {
-    document.getElementById('home').classList.add('loaded');
-})
+
+    const link = document.querySelectorAll('#categories a');
+    link.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault();
+            link.forEach((link) => link.classList.remove('active'));
+            event.target.classList.add('active');
+        })
+    });
+
+    //Listener for pics
+    const overlay = document.getElementById('overlay');
+    document.querySelectorAll('.gallery img').forEach((pic) => {
+
+        pic.addEventListener('click', () => {
+            const route = pic.getAttribute('src');
+            overlay.classList.add('o-active');
+            document.querySelector('#overlay img').src = route;
+        });
+    });
+
+
+    //Listener button close pop-up
+    document.querySelector('#close-popup').addEventListener('click', () => {
+        overlay.classList.remove('o-active');
+    });
+
+    //Listener button close pop-up
+    overlay.addEventListener('click', (eventp) => {
+        eventp.target.id === 'overlay' ? overlay.classList.remove('o-active') : '';
+    });
+});
